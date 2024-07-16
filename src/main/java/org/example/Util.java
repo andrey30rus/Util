ff7174de6f9e313304d178c8d33e08c8f94fa682
AOvchinnikov10 <AOvchinnikov10@slb.com>
2024-07-16 12:59:54 +0400

    Initial commit

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class Util {
+    public static void main(String[] args) {
+        List<String> allArgs = List.of(args);
+//        System.out.println(Arrays.toString(args));
+        System.out.println(allArgs);
+
+
+        System.out.println("Hello world!");
+    }
+}
